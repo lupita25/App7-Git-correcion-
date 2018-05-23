076f66f0ef5bb478006c5fc4373b2c3cec849a39
andrade3546354 <adolfandrade1@gmail.com>
2018-05-23 15:01:04 +0200

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,20 +1,20 @@
-// JavaScript Document
+﻿// JavaScript Document
 
 $(document).ready(function(e) {
     document. addEventListener("deviceready",function(){
 		$('#izquierda').on("swipeleft", function(){
-			navigator.notificacion.alert("Deslizo hacia la izquierda",function(){"aplicacion 7","ok"});
+			navigator.notification.alert("Deslizo hacia la izquierda",function(){"aplicacion 7","ok"});
 		});//cerrar deslizar izquierda
-		$('#derecha').on("swiperinght", function(){
-			navigator.notificacion.confirm("¿Que quieres hacer?",function(op){
+		$('#derecha').on("swiperight", function(){
+			navigator.notification.confirm("¿Que quieres hacer?",function(op){
 				switch(op)
 				{
 					case 1:
-					navigator.notificacion.beep(1);
+					navigator.notification.beep(1);
 					break;
 					
 					case 2:
-					navigator.notificacion.vibrate(3000);
+					navigator.notification.vibrate(3000);
 					break;
 				}
 			},"aplicacion 7","Beepear, Vibrar, Cancelar");
